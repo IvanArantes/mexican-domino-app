90b26150600535822b1917e6652d29e68067b9ea
Pedro Yoda Saito <pedro.yoda@gmail.com>
2019-11-26 14:45:12 -0300

    primera versao tela ranking

---
+++ src/pages/Ranking/Ranking.styles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Dimensions} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+});
+
+export default styles;
