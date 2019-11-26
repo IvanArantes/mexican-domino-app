938b23c40414f42aeb352cda01c81c827a20b425
marcioper <marcioper@gmail.com>
2019-11-26 15:09:55 -0300

    Add helper function to storage data

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import Storage from '../../helpers/storage';
 
 import {
   Text,
@@ -38,31 +40,51 @@ const styles = StyleSheet.create({
   },
 });
 
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover">
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
+function Main() {
+  useEffect(() => {
+    const fncTest = async () => {
+      // Update the document title using the browser API
+      const arr = [
+        {name: 'Joao', score: 5},
+        {name: 'Marcia', score: 6},
+        {name: 'Jose', score: 7},
+      ];
+      Storage.storeItem('@dataList', arr);
+      const dataRet = await Storage.retrieveItem('@dataList');
+      console.tron.log(`Data: ${JSON.stringify(dataRet)}`);
+    };
+
+    fncTest();
+  }, []);
+
+  return (
+    <ImageBackground
       source={{
-        uri:
-          'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
+        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
       }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>
-      Essa é a tela principal da sua aplicação =)
-    </Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>
-      src/pages/Main/index.js
-    </Text>
-  </ImageBackground>
-);
+      style={styles.container}
+      resizeMode="cover">
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Image
+        source={{
+          uri:
+            'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
+        }}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
+      <Text style={styles.instructions}>
+        Essa é a tela principal da sua aplicação =)
+      </Text>
+      <Text style={styles.instructions}>
+        Você pode editar a tela no arquivo:
+      </Text>
+      <Text style={[styles.instructions, styles.fileName]}>
+        src/pages/Main/index.js
+      </Text>
+    </ImageBackground>
+  );
+}
 
 export default Main;
